6167fc5cc5378c27b37a179d1f71d5c586152481
Michael van Tellingen <michael@mvantellingen.nl>
2018-08-06 19:54:42 +0200

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 
 setup(
     name='spaken',
-    version='0.1.2',
+    version='0.1.3',
     description=description,
     url='https://github.com/labd/spaken',
     author="Michael van Tellingen",
